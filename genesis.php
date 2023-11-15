ac87a3737f1e28637f4102c94f354944ad6288e7
Scott W. Lee <scott.lee@comsparc.com>
2023-11-14 21:53:26 -0500

    initial startup

---
+++ genesis.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Plugin Name: Genesis
+ * Plugin URI:  https://applab.comsparc.com/project/genesis
+ * Author:      COMSPARC
+ * Author URI:  https://comsparc.com
+ * Description: This plugin is the <b>first</b> WordPress application developed by COMSPARC.
+ * Version:     1.0.0
+ * License:     GPL-3.0+
+ * License URL: https://www.gnu.org/licenses/gpl-3.0.txt
+ * text-domain: genesis
+*/
