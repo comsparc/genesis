c52e454bd03afb4ebea41ecb0aee547f24a4ff82
Scott W. Lee <scott.lee@comsparc.com>
2023-12-20 23:13:35 -0500

    code change

--- genesis.php
+++ genesis.php
@@ -75,7 +75,7 @@ if (class_exists('cGenesis')) {
 
 // 3 triggers of a plugin
 // Activation
-// require_once plugin_dir_path(__FILE__).'include/genesis-plugin-activate.php'
+require_once plugin_dir_path(__FILE__).'include/genesis-plugin-activate.php';
 
 // registration hook to this file using function activate from class instance vGenesis
 // calling static method without creating instance of a class
