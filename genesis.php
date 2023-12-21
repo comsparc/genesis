54095a8fbae88c3bae7ac057130f67495c3968d7
Scott W. Lee <scott.lee@comsparc.com>
2023-12-20 22:34:09 -0500

    test again

--- genesis.php
+++ genesis.php
@@ -82,8 +82,8 @@ if (class_exists('cGenesis')) {
 // register_activation_hook(__FILE__, array ('GenesisPluginActivate','activate'));
 
 // Deactivation
-require_once plugin_dir_path(__FILE__).'include/genesis-plugin-deactivate.php'
-register_deactivation_hook(__FILE__, array ('GenesisPluginDeactivate','deactivate'));
+// require_once plugin_dir_path(__FILE__).'include/genesis-plugin-deactivate.php'
+// register_deactivation_hook(__FILE__, array ('GenesisPluginDeactivate','deactivate'));
 
 // uninstall
 // register_uninstall_hook(__FILE__, array ($vGenesis,'uninstall'));
