0bc90305310715722d8ef0a68614c9b714725e54
Scott W. Lee <scott.lee@comsparc.com>
2024-01-25 22:44:24 -0500

    position

--- genesis.php
+++ genesis.php
@@ -43,7 +43,7 @@ class cGenesis {
     }
 
     public function add_admin_pages(){
-        add_menu_page('Genesis', 'Genesis Tool', 'manage_options', 'csc-genesis', array($this, 'admin_index'), 'dashicons-cover-image', 10);
+        add_menu_page('Genesis', 'Genesis Tool', 'manage_options', 'csc-genesis', array($this, 'admin_index'), 'dashicons-cover-image', 100);
     }
 
     public function admin_index(){
