795a6d5f57d426ded0ff1b2f213bb4a41d62f326
Scott W. Lee <scott.lee@comsparc.com>
2024-01-29 22:44:30 -0500

    composer 4

--- genesis.php
+++ genesis.php
@@ -17,10 +17,6 @@
 
 defined('ABSPATH') or die("0"); // Kill program if entry doesn't have ABSPATH defined
 
-// if (file_exists(dirname( __FILE__ ) . '/vendor/autoload.php')) {
-//     require_once dirname( __FILE__ ) . '/vendor/autoload.php';
-// }
-
 class cGenesis {
 
     public $pluginName;
