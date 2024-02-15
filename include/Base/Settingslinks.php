71c1e2925c42f871608b919d3f6d6e693fd06bd7
Scott W. Lee <scott.lee@comsparc.com>
2024-02-14 21:48:07 -0500

    test again

--- include/Base/Settingslinks.php
+++ include/Base/Settingslinks.php
@@ -7,13 +7,13 @@
  
  class SettingsLinks {
     // set settings link on the plugin page
-    public function register() {
-          add_filter("plugin_action_links_".PLUGIN, array($this, 'settings_link'));
-    }
+    // public function register() {
+    //       add_filter("plugin_action_links_".PLUGIN, array($this, 'settings_link'));
+    // }
 
-    public function settings_link($links) {
-        $settings_link = '<a href="admin.php?page=genesis">Settings</a>';
-        array_push($links, $settings_link);
-        return $links;
-    }
+    // public function settings_link($links) {
+    //     $settings_link = '<a href="admin.php?page=genesis">Settings</a>';
+    //     array_push($links, $settings_link);
+    //     return $links;
+    // }
  }
