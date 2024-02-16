2bf49c506fe4af7b548b04d5f0a01ad46d13c21f
Scott W. Lee <scott.lee@comsparc.com>
2024-02-15 22:50:08 -0500

    test

--- include/Init.php
+++ include/Init.php
@@ -13,7 +13,6 @@
    public static function get_services(){
       return [
          Pages\Admin::class, // return list of classes without creating instances
-         Base\SettingsLinks::class,
          Base\Enqueue::class
       ];
 
