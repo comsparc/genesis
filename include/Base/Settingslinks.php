1f659c4fb74a0cba815c5355ec1c707de7eb5d86
Scott W. Lee <scott.lee@comsparc.com>
2024-02-15 21:57:17 -0500

    test work this time

--- include/Base/Settingslinks.php
+++ include/Base/Settingslinks.php
@@ -8,8 +8,7 @@
  class SettingsLinks {
     // set settings link on the plugin page
     public function register() {
-        add_action('admin_menu', array($this, 'add_admin_pages'));
-        // add_filter("plugin_action_links_$this->plugin", array($this, 'settings_link'));
+        add_filter("plugin_action_links".PLUGIN, array($this, 'settings_link'));
     }
 
     public function settings_link($links) {
