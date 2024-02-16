1f1a03640d0851c913f380fe3f7621b5cecb2786
Scott W. Lee <scott.lee@comsparc.com>
2024-02-15 22:01:39 -0500

    test

--- include/Base/Settingslinks.php
+++ include/Base/Settingslinks.php
@@ -8,7 +8,7 @@
  class SettingsLinks {
     // set settings link on the plugin page
     public function register() {
-        add_filter("plugin_action_links".PLUGIN, array($this, 'settings_link'));
+        add_filter("plugin_action_links_".PLUGIN, array($this, 'settings_link'));
     }
 
     public function settings_link($links) {
